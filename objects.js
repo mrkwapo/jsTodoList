163e7c6f6a19a304645456f2c15110b9299aef6f
Nathaniel Kwapo <nathanielkwapo.student@careerdevs.com>
2019-03-29 00:54:14 +0000

    created objects.js

---
+++ objects.js
@@ -0,0 +1,22 @@
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {
+        console.log('My Todos:', this.todos)
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+
+    changeTodos: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+
+
+};
